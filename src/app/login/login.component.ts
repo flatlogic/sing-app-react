d186d1e5a3c3a24171342c524fdc1a21d03c409f
okendoken <philip@flatlogic.com>
2016-09-16 17:04:57 +0300

    fix(login): styles

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'login',
   styleUrls: [ './login.style.scss' ],
-  templateUrl: './login.template.html'
+  templateUrl: './login.template.html',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'login-page app'
+  }
 })
 export class Login {
   constructor() {
