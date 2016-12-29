694d4f1a86c83ff868cd2e4be71716a1a6ab8705
shoudaos <shoudaos92@gmail.com>
2016-12-29 17:08:32 +0300

    feat(layout): fix ng2-bootstrap path

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,7 +3,7 @@ import 'jquery-slimscroll';
 import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
 import { FormsModule }  from '@angular/forms';
-import { TooltipModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { TooltipModule } from 'ng2-bootstrap';
 
 import { ROUTES }       from './layout.routes';
 
