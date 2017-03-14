fe241bba0b203571cea543987b726cda7d5e5616
DimaSavchenko <dimasauchanka@gmail.com>
2017-03-14 15:07:43 +0300

    fix(badge): use bootstarp4 badge, remove tag

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -52,7 +52,7 @@ export class Layout {
         '.list-group-item:first-child:not(.js-notification-added)')
         .addClass('active js-notification-added')
         .find('.fa-circle')
-        .after('<span class="badge tag-danger ' +
+        .after('<span class="badge badge-danger badge-pill ' +
           'pull-right animated bounceInDown">3</span>');
     }, 1000);
   }
