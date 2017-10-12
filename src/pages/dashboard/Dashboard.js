a4ddfc16e2a4f09f0ba4e2d852ea691abaff2cd0
okendoken <philip@flatlogic.com>
2017-10-12 15:35:22 +0300

    feat: dashboard widgster fullscreen

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -15,7 +15,7 @@ class Dashboard extends React.Component {
         <h1 className="page-title">Dashboard <small><small>The Lucky One</small></small></h1>
         <Row>
           <Col md={6}>
-            <Widget title={<h5>Example <span className="fw-semi-bold">Widget</span></h5>}>
+            <Widget title={<h5>Example <span className="fw-semi-bold">Widget</span></h5>} fullscreen>
               <img className="float-left mr-sm" src={react} alt="React JS" width="80" />
               <p className="lead">You are looking at a completely new version of Sing App built
               with <strong>React JS</strong></p>
