291ef502dc1939bcc1d55728b12e4b0474715659
Ilya Kulgavy <i.kulgavy@gmail.com>
2018-10-24 13:31:35 +0300

    move to create-react-app

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Switch, Route, Redirect } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+
+/* eslint-disable */
+import ErrorPage from '../pages/error';
+/* eslint-enable */
+
+import LayoutComponent from '../components/Layout';
+import LoginComponent from '../pages/login';
+import '../styles/theme.scss';
+
+const PrivateRoute = ({ component, ...rest }) => {
+  return ( // eslint-disable-line
+  <Route
+    {...rest} render={props => (
+    localStorage.getItem('id_token') ? (
+      React.createElement(component, props)
+    ) : (
+      <Redirect
+        to={{
+          pathname: '/login',
+          state: { from: props.location }, // eslint-disable-line
+        }}
+      />
+    )
+  )}
+  />
+);
+}
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact render={() => <Redirect to="/app/main" />} />
+          <Route path="/app" exact render={() => <Redirect to="/app/main" />} />
+          <PrivateRoute path="/app" component={LayoutComponent} />
+          <Route path="/login" exact component={LoginComponent} />
+          <Redirect from="*" to="/app/main/analytics" />
+          <Route component={ErrorPage} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated,
+})
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
+import 'expose-loader?jQuery!jquery' // eslint-disable-line
+import 'expose-loader?$!jquery' // eslint-disable-line
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'
+import ReduxThunk from 'redux-thunk'
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+import * as $ from 'jquery'
+window.jQuery = window.$ = $
+
+const store = createStore(
+  reducers,
+  applyMiddleware(ReduxThunk)
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
