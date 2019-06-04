19a0f607130b8f5d6051a3f6bd3f26c1c01a26ed
shoudaos <shoudaos92@gmail.com>
2019-06-04 15:25:44 +0300

    feat(config): updated backend endpoint; removed hash from images names on prod

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const hostApi = process.env.NODE_ENV === "development" ? "http://b74dcef5.ngrok.io" : "https://sing-app-node.herokuapp.com";
-const portApi = process.env.NODE_ENV === "development" ? "" : "";
+const hostApi = process.env.NODE_ENV === "development" ? "http://localhost" : "https://flatlogic-node-backend.herokuapp.com";
+const portApi = process.env.NODE_ENV === "development" ? 8080 : "";
 const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}`;
 
 export default {
