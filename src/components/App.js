e318a8d84e4188ba105c427c72aa133eb3661cb4
Vlad <excelent91@mail.ru>
2019-12-10 13:45:32 +0200

    virtual-tour completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router';
 import { HashRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import Joyride, { STATUS } from 'react-joyride';
 
 /* eslint-disable */
 import ErrorPage from '../pages/error';
@@ -30,59 +29,6 @@ const PrivateRoute = ({dispatch, component, ...rest }) => {
 const CloseButton = ({closeToast}) => <i onClick={closeToast} className="la la-close notifications-close"/>
 
 class App extends React.PureComponent {
-
-  state = {
-    run: true,
-    steps: [
-      {
-        content: 'Tabs or spaces? 🤔',
-        placement: 'bottom',
-        target: '#toggleSidebar',
-        textAlign: 'center',
-      },
-      {
-        content: "A button! That's rare on the web",
-        placement: 'bottom',
-        target: '.dropdown-toggle',
-      },
-      {
-        content: "Sometimes I wonder what's inside my mind",
-        placement: 'bottom',
-        target: '#toggle-chat',
-      },
-      {
-        content: 'Modal, Portal, Quintal!',
-        placement: 'bottom',
-        target: '.tutorial-dropdown',
-      },
-      {
-        content: 'Modal, Portal, Quintal!',
-        placement: 'left',
-        target: '.helper-button'
-      },
-    ],
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (!prevState.modalIsOpen && this.state.modalIsOpen) {
-      this.start();
-    }
-  }
-
-  handleJoyrideCallback = (CallBackProps) => {
-    const { status } = CallBackProps;
-
-    if (([STATUS.FINISHED, STATUS.SKIPPED]).includes(status)) {
-      this.setState({ run: false });
-    }
-
-  };
-
-  start = () => {
-    this.setState({
-      run: true,
-    });
-  };
   
   render() {
     return (
@@ -92,26 +38,6 @@ class App extends React.PureComponent {
                 hideProgressBar
                 closeButton={<CloseButton/>}
             />
-        <Joyride
-          callback={this.handleJoyrideCallback}
-          continuous={true}
-          run={this.state.run}
-          showSkipButton={true}
-          steps={this.state.steps}
-          spotlightPadding={-10}
-          disableOverlay={true}
-          styles={{
-            options: {
-              arrowColor: '#ffffff',
-              backgroundColor: '#ffffff',
-              overlayColor: 'rgba(79, 26, 0, 0.4)',
-              primaryColor: '#000',
-              textColor: '#495057',
-              spotlightPadding: 0,
-              zIndex: 1000
-            },
-          }}
-        />
             <HashRouter>
                 <Switch>
                     <Route path="/" exact render={() => <Redirect to="/app/main"/>}/>
--- src/components/Helper/Helper.js
+++ src/components/Helper/Helper.js
@@ -28,29 +28,29 @@ class Helper extends Component {
     run: false,
     steps: [
       {
-        content: 'Tabs or spaces? 🤔',
+        content: 'Easily adjust navbar 🤔',
         placement: 'left',
         target: '.navbar-type-switcher',
         textAlign: 'center',
         disableBeacon: true,
       },
       {
-        content: "A button! That's rare on the web",
+        content: "Choose a color for navbar, create unique layout ✌️",
         placement: 'left',
         target: '.navbar-color-picker',
       },
       {
-        content: "Sometimes I wonder what's inside my mind",
+        content: "Also customize sidebar type, it's cool 👌",
         placement: 'left',
         target: '.sidebar-type-switcher',
       },
       {
-        content: 'Modal, Portal, Quintal!',
+        content: 'We also have different colors for sidebar, pick one from palette 😇',
         placement: 'left',
         target: '.sidebar-color-picker',
       },
       {
-        content: 'Modal, Portal, Quintal!',
+        content: 'Purchase out template if you like it, we appreciate it 🤗!',
         placement: 'left',
         target: '.purchase-button'
       },
@@ -114,7 +114,6 @@ class Helper extends Component {
           run={this.state.run}
           showSkipButton={true}
           steps={this.state.steps}
-          spotlightPadding={-10}
           disableOverlay={true}
           styles={{
             options: {
@@ -124,7 +123,41 @@ class Helper extends Component {
               primaryColor: '#000',
               textColor: '#495057',
               spotlightPadding: 0,
-              zIndex: 10000
+              zIndex: 1000,
+              padding: 5,
+              width: 240,
+            },
+            tooltip: {
+              fontSize: 15,
+              padding: 5,
+            },
+            tooltipContent: {
+              padding: '20px 5px 0',
+            },
+            floater: {
+              arrow: {
+                padding: 10
+              },
+            },
+            buttonClose: {
+              display: 'none'
+            },
+            buttonNext: {
+              backgroundColor: "#FFC247",
+              fontSize: 13,
+              borderRadius: 4,
+              color: "#ffffff",
+              fontWeight: "bold"
+            },
+            buttonBack: {
+              color: "#798892",
+              marginLeft: 'auto',
+              fontSize: 13,
+              marginRight: 5,
+            },
+            buttonSkip: {
+              color: "#798892",
+              fontSize: 13,
             },
           }}
         />
@@ -139,13 +172,13 @@ class Helper extends Component {
           className={s.themeHelperContent}
         >
           <div className={s.helperHeader}>
-            <h5>Theme</h5>
+            <h5 className="m-0">Theme</h5>
             <Button onClick={this.start} outline color="info">Check out tour!</Button>  
           </div>    
 
           <div className="theme-settings">
-            <h5>Navbar Type</h5>
-            <div className="form-group row navbar-type-switcher">
+            <h5 className="navbar-type-switcher">Navbar Type</h5>
+            <div className="form-group row">
               <div className="abc-radio">
                 <input onChange={() => this.navbarStateToggle(NavbarTypes.STATIC)} type="radio" checked={navbarType === NavbarTypes.STATIC ? true : ''} name="navbar-type" id="navbar_static" />
                 <label htmlFor="navbar_static">Static</label>
@@ -165,8 +198,8 @@ class Helper extends Component {
               customizationItem={"navbar"}
             />
 
-            <h5 className="mt-4">Sidebar Type</h5>
-            <div className="form-group row sidebar-type-switcher">
+            <h5 className="mt-4 sidebar-type-switcher">Sidebar Type</h5>
+            <div className="form-group row">
               <div className="abc-radio">
                 <input type="radio" onChange={() => this.sidebarStateToggle(SidebarTypes.TRANSPARENT)} checked={sidebarType === SidebarTypes.TRANSPARENT ? true : ''} name="sidebar-type" id="sidebar_transparent" />
                 <label htmlFor="sidebar_transparent">Transparent</label>
